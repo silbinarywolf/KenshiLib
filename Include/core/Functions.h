a9f50231829b88e7790ce531c0ebb2a3a79c0c57
Jae B <doogie1012@gmail.com>
2026-09-23 21:03:08 +1000

    add compile-time error if _MSC_VER not set or _MSC_VER != 1600

--- Include/core/Functions.h
+++ Include/core/Functions.h
@@ -3,6 +3,17 @@
 #include <Defines.h>
 #include <stdint.h>
 
+// Why do this check?
+// ------------------
+// Using the wrong toolchain could mean you end up with memory corruption/crashing bugs in your mod.
+//
+// - std::string is 40 bytes for MSVC 2010. For Clang it can be 24 bytes or for other MSVC versions it can be 32 bytes.
+// - std::vector is 32 bytes for MSVC 2010. For Clang/other MSVC versions it can be 24 bytes.
+// - std::map is 32 bytes for MSVC 2010. For Clang it can be 24 bytes or for other MSVC versions it can be 16 bytes.
+#if !defined(_MSC_VER) || _MSC_VER != 1600
+#error "RE_Kenshi mod must be built using Microsoft Visual Studio 2010 toolchain. Due to C++ version incompatibility, size differences in std::string, std::vector and std::map can cause memory corruption/crashing bugs."
+#endif
+
 namespace KenshiLib
 {
 	KLIB_EXPORT intptr_t GetRealAddress(void* fun);
